07efd3b3e0b10fc3eb582512f193313c8a2bf279
Mohsen <mohsen@Mohsens-MacBook-Pro.local>
2020-02-25 19:00:59 +0330

    Profiles and locations apps created

--- coronacore/settings.py
+++ coronacore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'coronacore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'postgis',
+        'USER': 'mohsen',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '25432'
     }
 }
 
