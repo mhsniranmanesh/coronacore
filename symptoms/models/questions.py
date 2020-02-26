586fa6431c0635f9ea0466c669858115627bda07
Mohsen <mohsen@Mohsens-MacBook-Pro.local>
2020-02-26 19:45:16 +0330

    Question models added to symptoms app

---
+++ symptoms/models/questions.py
@@ -0,0 +1,25 @@
+from django.contrib.gis.db import models
+import uuid as uuid_lib
+
+
+class Question(models.Model):
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    text = models.CharField(max_length=10000, blank=False)
+
+
+class QuestionDetails(models.Model):
+    MEDIA_TYPE_CHOICES = (
+        ('video', 'Video'),
+        ('image', 'Image'),
+        ('none', 'None')
+    )
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    media_type = models.CharField(blank=True, choices=MEDIA_TYPE_CHOICES, max_length=10)
+    url = models.URLField(max_length=1000, null=True)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+
+
+class QuestionOptions(models.Model):
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    text = models.CharField(max_length=10000, blank=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="question_options")
